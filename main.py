4e3c3da6e4b0ef8ea6de1b0d6ea5efc9167e7ce0
CSJLU <lucourage@gmail.com>
2023-09-14 19:46:54 -0400

    Setup flask and configurate important information needed to send emails

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+from flask_mail import Mail, Message
+
+#obtain password from user directory
+with open("app_password.txt", "r", encoding="utf-8") as f:
+    password = f.read()
+
+#obtain sender email from user directory
+with open("sender_email.txt", "r", encoding="utf-8") as f:
+    sender = f.read()
+
+
+#important app information necessary to send emails
+app = Flask(__name__)
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = sender
+app.config['MAIL_PASSWORD'] = password
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+email = Mail(app)
+
+#UI user encounters upon entering website
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
