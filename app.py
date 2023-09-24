e72553950fe18a6a262f42755f5f320d5801b1a5
CSJLU <lucourage@gmail.com>
2023-09-24 18:02:55 -0400

    Comments for future code

--- app.py
+++ app.py
@@ -15,6 +15,14 @@ def send_email():
 
     return render_template('success.html')
 
+#create sqlite3 database and put email from request into database
+
+#for each email in database, go through it and send email 
+
+#function to send email, incorporate this with the above comment
+
+#schedule email sender (use the function to send email to make it a daily automated email sender using a time schedule)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
